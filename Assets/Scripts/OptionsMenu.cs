489743c52089b4a5105e113311b025f2cd8d36da
Shizobear <K.Tobi@outlook.de>
2019-02-23 16:24:57 +0100

    added Mainmenu and OptionsMenu

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public AudioMixer audioMixer;
+	public void SetVolume(float value) {
+		
+		audioMixer.SetFloat("Volume", value);
+
+	}
+}
