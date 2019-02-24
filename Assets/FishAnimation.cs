a09af147b815b722e156f6a2bf5f27dac44f2142
Shizobear <K.Tobi@outlook.de>
2019-02-24 18:40:51 +0100

    added Fish Animations

---
+++ Assets/FishAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	private Animator anim;
+	private Fish player;
+	private Vector3 direction;
+	void Start () {
+		anim = this.GetComponent<Animator>();
+		player = Fish.GetReference();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = player.GetCurrentDirection();
+		anim.SetFloat("directionX", direction.x);
+		anim.SetFloat("directionY", direction.y);
+
+	}
+}
