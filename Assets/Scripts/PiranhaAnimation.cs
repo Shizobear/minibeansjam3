d401a65a7451ceb5f6a83127da793cde2d86dd2a
Shizobear <K.Tobi@outlook.de>
2019-02-24 17:41:22 +0100

    piranha animations!!!!

---
+++ Assets/Scripts/PiranhaAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiranhaAnimation : MonoBehaviour {
+
+	private Animator anim;
+	private Piranha piranha;
+	private Vector3 direction;
+	void Start () {
+		anim = this.GetComponent<Animator>();
+		piranha = this.GetComponent<Piranha>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = piranha.GetCurrentDirection();
+		anim.SetFloat("directionX", direction.x);
+		anim.SetFloat("directionY", direction.y);
+
+	}
+}
